53332a68f2075a233411651fcb034ab9101dbe44
pullyl <pullyl@gmail.com>
2017-01-14 15:07:12 -0500

    Fixing lat and long calculations

--- helper_scripts/combine_files.py
+++ helper_scripts/combine_files.py
@@ -26,6 +26,23 @@ directoryFiles = [f for f in listdir(path_to_folder) if isfile(join(path_to_fold
 #Read in congresspeople data
 congress_data = pd.read_csv(congress_data_file, encoding='utf8')
 
+def convert_to_lat(input):
+    input = str(input)
+    if input == "nan":
+        return ""
+
+    input2 = input.replace("{u'type': u'Point', u'coordinates': [", "").replace("]}", "").split(",")
+    return input2[0]
+
+def convert_to_long(input):
+    input = str(input)
+    if input == "nan":
+        return ""
+
+    input2 = input.replace("{u'type': u'Point', u'coordinates': [", "").replace("]}", "").split(",")
+    return input2[1]
+
+
 for file in directoryFiles:
     if file_suffix not in file:
         continue
@@ -36,6 +53,9 @@ for file in directoryFiles:
     file_name = "%s/%s" % (path_to_folder, file)
     df = pd.read_csv(file_name, encoding='utf8')
     df[u'twitter'] = twitter_handle
+    df['lat'] = df['coordinates'].apply(convert_to_lat)
+    df['long'] = df['coordinates'].apply(convert_to_long)
+    del df['coordinates']
 
     #try to merge with extracted data
     extracted_data = congress_data.loc[congress_data['twitter'] == twitter_handle]
