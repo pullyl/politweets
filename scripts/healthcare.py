61f59129f130b169b7926821a31c2e862c859d13
pullyl <pullyl@gmail.com>
2017-04-16 15:16:45 -0400

    Modifications to healthcare script
    Adding in party, adding some terms, fixing some bugs

--- scripts/healthcare.py
+++ scripts/healthcare.py
@@ -4,40 +4,79 @@ from os.path import isfile, join
 import pandas as pd
 import yaml
 
-healthcare_terms = ['ACA', 'Affordable Care Act', 'Obama Care', 'ObamaCare', 'Trump Care', ' health ', 'health care',
-                    'medicare', 'medicaid', 'medial', 'health insurance', 'american health care act', 'acha']
+healthcare_terms = ['ACA ', 'Affordable Care Act', 'Obama Care', 'ObamaCare', 'Trump Care', 'TrumpCare', ' health ', 'health care',
+                    'medicare', 'medicaid', 'medical', 'health insurance', 'american health care act', 'acha', 'healthcare']
+r_leaning = ['Obama Care', 'ObamaCare', 'american health care act', 'acha']
+d_leaning = ['ACA', 'Affordable Care Act', 'Trump Care', 'TrumpCare']
 path_to_folder = "../raw_data"
 file_suffix = "_tweets.csv"
 output_file = "../manipulated_data/healthcare_output.csv"
 social_yaml = '../raw_data/legislators-social-media.yaml'
+legislators_yaml = '../raw_data/legislators-current.yaml'
+
 
 twitter = 'twitter'
 social = 'social'
 id = 'id'
 bioguide = 'bioguide'
+party = 'party'
+terms = 'terms'
 
 def simplify_text(input):
     text = input.replace("\n", "")
     return text
 
 def is_healthcare(input):
-    for term in healthcare_terms:
-        if input.find(term) == True:
-            return True
+    return contains_term(healthcare_terms, input)
 
-    return False
+def is_r_leaning(input):
+    return contains_term(r_leaning, input)
+
+def is_d_leaning(input):
+    return contains_term(d_leaning, input)
+
+def contains_term(terms, input):
+    for term in terms:
+        if not input.lower().find(term.lower()) == -1:
+            return 1
+
+    return 0
+
+def healthcare_count(input):
+    count = 0
+    for term in healthcare_terms:
+        if not input.lower().find(term.lower()) == -1:
+            count += 1
+
+    return count
+
+def contains_word(input, args):
+    if not input.lower().find(args.lower()) == -1:
+        return 1
+
+    return 0
 
 def main():
     print('Analyzing tweets related to healthcare')
 
-    #load in yaml
+    #load in social yaml
     with open(social_yaml, 'r') as stream:
         data_loaded = yaml.load(stream)
-
-    ids = {}
+    twitter_dict = {}
+    bio_dict = {}
     for data in data_loaded:
         if twitter in data[social].keys():
-            ids[data[social][twitter]] = data[id][bioguide]
+            twitter_dict[data[social][twitter].lower()] = {bioguide: data[id][bioguide]}
+            bio_dict[data[id][bioguide]] = data[social][twitter].lower()
+
+    # load in legislators yaml
+    with open(legislators_yaml, 'r') as stream:
+        data_loaded = yaml.load(stream)
+        for data in data_loaded:
+            if party in data[terms][0].keys():
+                if data[id][bioguide] in bio_dict.keys():
+                    twitter_dict[bio_dict[data[id][bioguide]]][party] = data[terms][0][party]
+
 
     #setting up constants
     first_write = True
@@ -45,25 +84,41 @@ def main():
     directoryFiles = [f for f in listdir(path_to_folder) if isfile(join(path_to_folder, f))]
     num_files = 0
 
+    row_count = 0
     for file in directoryFiles:
         if file_suffix not in file:
             continue
+
+        #if 'PeteSessions' not in file:
+        #    continue
 
         num_files += 1
         #if num_files > 5:
         #    break
 
-        twitter_handle = file.replace(file_suffix, "")
+        twitter_handle = file.replace(file_suffix, "").lower()
         print("processing %s with twitter handle: %s" % (file, twitter_handle))
         file_name = "%s/%s" % (path_to_folder, file)
         df = pd.read_csv(file_name, encoding='utf8', quotechar='"')
-        df[u'twitter'] = twitter_handle
-        df[u'bioguide'] = ids[twitter_handle]
+        df = df.rename(columns={'user': 'twitter'})
         df['text'] = df['text'].apply(simplify_text)
         df['healthcare'] = df['text'].apply(is_healthcare)
+        df['healthcare_count'] = df['text'].apply(healthcare_count)
+        df['r_leaning'] = df['text'].apply(is_r_leaning)
+        df['d_leaning'] = df['text'].apply(is_d_leaning)
+        for term in healthcare_terms:
+            df[term] = df['text'].apply(contains_word, args={term})
+        if twitter_handle in twitter_dict.keys():
+            df['party'] = twitter_dict[twitter_handle][party]
+            df['bioguide'] = twitter_dict[twitter_handle][bioguide]
+        else:
+            print 'inspect this'
+
 
         #remove rows without healthcare
-        df = df.ix[~(df['healthcare'] == False)]
+        df = df.ix[~(df['healthcare'] == 0)]
+
+        row_count += len(df.index)
 
         # write the data to a file
         if first_write:
@@ -72,8 +127,17 @@ def main():
         else:
             df.to_csv(output_file, encoding='utf8', mode='a', header=False)
 
-
-
+    print('just wrote %d rows' % (row_count))
 
 
 main()
+
+def test():
+    #text = "Medicare &amp; Medicaid"
+    text = ""
+    print 'is healthcare: %d' % (is_healthcare(text))
+
+    for term in healthcare_terms:
+        print term
+        print( contains_word(text, term) )
+#test()
